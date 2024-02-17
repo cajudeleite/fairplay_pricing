6092e198f06dea31b44e8bb143dfbd2485209712
Nathan Debaz <nathandebaz266@gmail.com>
2024-02-17 16:13:22 -0300

    pip freeze

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import scikitlearn as sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
